017beae9f3788c24f4c7cbd4e9c72099204db20a
Tom Nicklin <t-nicklin@hotmail.co.uk>
2015-03-10 20:21:03 +0000

    After Ade had a meltdown and went to the gym

---
+++ Exercise3/src/ex4/Part1Grid.java
@@ -0,0 +1,91 @@
+package ex4;
+import lejos.geom.Line;
+import lejos.geom.Point;
+import lejos.robotics.navigation.Pose;
+import rp.robotics.mapping.IGridMap;
+import rp.robotics.mapping.RPLineMap;
+
+public class Part1Grid implements IGridMap{
+
+	RPLineMap lineMap;
+	int gridXSize;
+	int gridYSize;
+	float xStart;
+	float yStart;
+	float cellSize;
+	
+	
+	public Part1Grid(RPLineMap _lineMap, int _gridXSize,
+			int _gridYSize, float _xStart, float _yStart, float _cellSize){
+		lineMap =_lineMap;
+		gridXSize = _gridXSize;
+		xStart = _xStart;
+		yStart = _yStart;
+		cellSize=_cellSize;
+		
+	}
+	
+	@Override
+	public int getXSize() {
+		return gridXSize;
+	}
+
+	@Override
+	public int getYSize() {
+		return gridYSize;
+	}
+
+	@Override
+	public boolean isValidGridPosition(int _x, int _y) {
+		return ((_x > 0 && _x< gridXSize)  &&  (_y >0 && _y<gridYSize));
+	}
+
+	@Override
+	public boolean isObstructed(int _x, int _y) {
+		Point p =new Point(_x, _y);
+		return lineMap.inside(p);
+	}
+
+	@Override
+	public Point getCoordinatesOfGridPosition(int _x, int _y) {
+		float x = (this.cellSize * _x)  +xStart;
+		float y = (this.cellSize * _y) + yStart;
+		Point p = new Point(x, y);
+		return p;
+	}
+
+	@Override
+	public boolean isValidTransition(int _x1, int _y1, int _x2, int _y2) {
+		//convert x and y to line map points
+		//draw direct line between start and finish points
+		//loop through all obstacles and check for intersection
+		float xStarting = _x1 * this.cellSize + xStart;
+		float yStarting = _y1 * this.cellSize + yStart;
+		float xEnd = _x2 * this.cellSize + xStart;
+		float yEnd = _y2 * this.cellSize + yStart;
+		
+		if(this.isValidGridPosition(_x1, _y1) && !this.isObstructed(_x1,_y1))  && // DOTH SAME FOR X2 AND Y2
+		
+		{
+			if(_x1 == _x2){
+				//FIND THE MIN VALUE BETWEEN THE TWO Y'S (THE FLOAT VERSIONS) AND START A LOOP FROM THERE UP UNTIL THE HIGHER VALUE
+				//WHILE LOOPING CHECK EACH POINT IF THE POINT IS INSIDE
+				//IF ALL ARE TRUE RETURN TRUE, ELSE RETURN FALSE
+			}
+			else if (_y1 == _y2){
+				
+			}
+		}
+		
+		
+		return false;
+	}
+
+	@Override
+	public float rangeToObstacleFromGridPosition(int _x, int _y, float _heading) {
+		Pose p = new Pose(_x,_y,_heading);
+		return lineMap.range(p);
+	}
+
+	
+}
